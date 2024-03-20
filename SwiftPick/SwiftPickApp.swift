7ea91971c756f2a556f8b4d0e65e95ea89b0e875
Kgosi Rasebitse <kgosi.rasebitse@rain.co.za>
2024-03-20 12:20:39 +0200

    Initial Commit

---
+++ SwiftPick/SwiftPickApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPickApp.swift
+//  SwiftPick
+//
+//  Created by Kgosi Rasebitse on 2024/03/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftPickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
